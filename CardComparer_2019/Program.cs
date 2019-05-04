b90f07a5e350ab33b535fdf4e721a389ee7f3fcf
Mark <Mark@DESKTOP-16DN5B4>
2019-05-04 12:08:47 +0200

    Write 5 random cards to console

--- CardComparer_2019/Program.cs
+++ CardComparer_2019/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CardComparer_2019
 {
     class Program
     {
+        private static List<Card> deck = new List<Card>();
+        private static Random random;
+
         static void Main(string[] args)
         {
+            createFiveRandomCards();
+            foreach (var card in deck)
+            {
+                Console.WriteLine(card.Name);
+            }
+            Console.ReadLine();
+        }
+
+        private static void createFiveRandomCards()
+        {
+            random = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                Card card = new Card()
+                {
+                    Value = (Values)random.Next(1, 14),
+                    Suit = (Suits)random.Next(1,4)
+                };
+                deck.Add(card);
+            }
         }
     }
 }
