3d1f4a67e0fa7268cce86bab08ef0d2383b7778b
Mark <Mark@DESKTOP-16DN5B4>
2019-05-04 11:48:08 +0200

    first commit - basic card class

---
+++ CardComparer_2019/Card.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardComparer_2019
+{
+    public class Card
+    {
+        public Suits Suit { get; set; }
+        public Values Value { get; set; }
+
+        public string Name
+        {
+            get
+            {
+                return $"{Value.ToString()} of {Suit.ToString()}"; 
+            }
+        }
+    }
+}
